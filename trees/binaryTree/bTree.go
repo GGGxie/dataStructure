ccffc853093c607d183bad0d90e3855f4f571191
xzg <790102426@qq.com>
2021-05-16 23:53:12 +0800

    update

--- trees/binaryTree/bTree.go
+++ trees/binaryTree/bTree.go
@@ -6,18 +6,22 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+//找出二叉树中某两个节点的第一个共同祖先
 func lowestCommonAncestor(root *TreeNode, p *TreeNode, q *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
-	if node := lowestCommonAncestor(root.Left, p, q); node != nil {
-		return node
-	}
 	if root == p || root == q {
 		return root
 	}
-	if node := lowestCommonAncestor(root.Right, p, q); node != nil {
-		return node
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+	if left != nil && right != nil {
+		return root
+	} else if left != nil {
+		return left
+	} else if right != nil {
+		return right
 	}
 	return nil
 }
