178f387fb44162e86a1267d96e5f56ca8a44e816
ex_xiecg <790102426@qq.com>
2021-06-04 18:44:45 +0800

    update

--- bits/bit.go
+++ bits/bit.go
@@ -1,6 +1,8 @@
 package bits
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //用M去填充N的第i-j位
 // 输入：N = 1024(10000000000), M = 19(10011), i = 2, j = 6
@@ -13,4 +15,30 @@ func InsertBits(N int, M int, i int, j int) int {
 	fmt.Printf("%016b\n", N)
 	N |= (M << i) //把N的第i-j位和M进行|运算
 	return N
+}
+
+//输出十进制小数的二进制表示
+func PrintBin(num float64) string {
+	var ret []byte
+	var count int
+	ret = append(ret, []byte{'0', '.'}...)
+	for ; count < 32; count++ {
+		temp := num * 2
+		if temp >= 1 {
+			ret = append(ret, '1')
+			num = temp - 1
+		} else {
+			ret = append(ret, '0')
+			num = temp
+		}
+		if num == 0 {
+			break
+		}
+	}
+
+	if count < 32 {
+		return string(ret)
+	} else {
+		return "ERROR"
+	}
 }
--- main.go
+++ main.go
@@ -5,13 +5,28 @@ import (
 )
 
 func main() {
-	var z int
-	z = 12
-	b := 4
-	fmt.Printf("%016b\n", 1<<2)
-	fmt.Printf("%016b\n", ^(1 << 2))
-	fmt.Printf("%016b\n", z)
-	fmt.Printf("%016b\n", z|(^(1 << 2)))
-	fmt.Printf("%016b\n", z&(^b))
+	fmt.Println(printBin(0.7))
+	// fmt.Println(1.423%10)
 	// fmt.Printf("%016b\n", bits.InsertBits(0b11111, 0b110, 1, 4))
+}
+func printBin(num float64) string {
+	var res []byte
+	res = append(res, '0')
+	res = append(res, '.')
+
+	for num != 0 {
+		num *= 2
+		fmt.Println(num)
+		if num >= 1 {
+			res = append(res, '1')
+			num -= 1
+		} else {
+			res = append(res, '0')
+		}
+	}
+	fmt.Println(string(res))
+	if len(res) >= 32 {
+		return "ERROR"
+	}
+	return string(res)
 }
