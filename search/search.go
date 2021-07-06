0bc21e06fdc9414f8e239d83199a3c9eda7a17c9
xzg <790102426@qq.com>
2021-07-06 22:31:37 +0800

    update

---
+++ search/search.go
@@ -0,0 +1,17 @@
+package search
+
+// 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+// 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+func isBadVersion(version int) bool
+func firstBadVersion(n int) int {
+	var start, end int
+	for start, end = 1, n; start < end; {
+		mid := (start + end) / 2
+		if isBadVersion(mid) {
+			end = mid
+		} else {
+			start = mid + 1
+		}
+	}
+	return start
+}
