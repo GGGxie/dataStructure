9d97813621869388edf170c1871599dc5513ac65
ex_xiecg <790102426@qq.com>
2021-09-16 18:40:51 +0800

    update

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-func main() {
-}
+import (
+	"fmt"
+)
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+func main() {
+	fmt.Println(wordBreak("leetcode", []string{"leet", "code"}))
 }
--- practice/string/string.go
+++ practice/string/string.go
@@ -1,6 +1,8 @@
 package string
 
-import "strings"
+import (
+	"strings"
+)
 
 // https://leetcode-cn.com/leetbook/read/top-interview-questions/xah8k6/
 // 判断字符串是否为回文串
@@ -31,64 +33,25 @@ func isLetterOrNumber(ch byte) bool { //判断是否为字母或数字
 	return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
 }
 
+// https://leetcode-cn.com/leetbook/read/top-interview-questions/xa503c/
+// 单词拆分。给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+// 状态转移方程：dp[i]=dp[j] && check(s[j..i−1])
 func wordBreak(s string, wordDict []string) bool {
-	t := NewTrie(wordDict)
-	t.Insert(s)
-	return t.Search(s)
-}
-
-//树的root为空,不储存数据
-type TrieTree struct {
-	Root *TrieNode
-}
-
-//树节点
-type TrieNode struct {
-	IsWord bool               //记录到该节点是否为一个单词
-	Next   map[rune]*TrieNode //记录下一个节点的信息
-}
-
-//新建字典树
-func NewTrie(arrList []string) *TrieTree {
-	trie := &TrieTree{}
-	trie.Root = &TrieNode{
-		Next:   make(map[rune]*TrieNode),
-		IsWord: false,
+	length := len(s)
+	mapp := make(map[string]bool)
+	for _, v := range wordDict {
+		mapp[v] = true
 	}
-	//插入单词
-	for _, value := range arrList {
-		trie.Insert(value)
-	}
-	return trie
-}
-
-//添加新单词
-func (t *TrieTree) Insert(word string) {
-	nowNode := t.Root
-	for _, v := range word {
-		if nowNode.Next[v] == nil { //子节点为空则添加新节点
-			newNode := &TrieNode{
-				Next:   make(map[rune]*TrieNode),
-				IsWord: false,
+	dp := make([]bool, length+1) //dp[i]记录子串s[0:i]是否能在字段中被拆分成一个或多个单词
+	dp[0] = true
+	for i := 1; i <= length; i++ {
+		for j := 0; j < i; j++ {
+			if dp[j] && mapp[s[j:i]] {
+				dp[i] = true
+				break
 			}
-			nowNode.Next[v] = newNode
 		}
-		//遍历下一个节点
-		nowNode = nowNode.Next[v]
 	}
-	//标记到达该节点为一个单词
-	nowNode.IsWord = true
-}
 
-//查找单词
-func (t *TrieTree) Search(word string) bool {
-	nowNode := t.Root
-	for _, v := range word {
-		if nowNode.Next[v] == nil { //遍历不下去则证明该单词不存在
-			return false
-		}
-		nowNode = nowNode.Next[v]
-	}
-	//返回该节点是否一个单词
-	return nowNode.IsWord
+	return dp[length]
 }
