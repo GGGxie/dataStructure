9407e44bba8f9f0bccdcdd29ce6420d3bd69a220
ex_xiecg <790102426@qq.com>
2021-09-27 15:06:55 +0800

    添加享元模式

--- main.go
+++ main.go
@@ -1,28 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
-func main() {
-	a := []int{1, 2, 3, 4, 5, 6, 7}
-	fmt.Printf("%p\n", &a)
-	rotate(a, 6)
-	fmt.Println(a)
+type Task struct {
+	id      int
+	randnum int
 }
-func rotate(nums []int, k int) {
-	length := len(nums)
-	k %= length //当k>length
-	index := length - k
-	copy(nums, append(nums[index:], nums[0:index]...))
+type Result struct {
+	task   Task
+	result int
 }
-func moveZeroes(nums []int) {
-	left, right, n := 0, 0, len(nums)
-	for right < n {
-		if nums[right] != 0 {
-			nums[left], nums[right] = nums[right], nums[left]
-			left++
-		}
-		right++
+
+var tasks = make(chan Task, 10)
+var results = make(chan Result, 10)
+
+func process(num int) int {
+	sum := 0
+	for num != 0 {
+		digit := num % 10
+		sum += digit
+		num /= 10
 	}
+	time.Sleep(2 * time.Second)
+	return sum
+}
+func worker(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for task := range tasks {
+		result := Result{task, process(task.randnum)}
+		results <- result
+	}
+}
+func createWorkerPool(numOfWorkers int) {
+	var wg sync.WaitGroup
+	for i := 0; i < numOfWorkers; i++ {
+		wg.Add(1)
+		go worker(&wg)
+	}
+	wg.Wait()
+	close(results)
+}
+func allocate(numOfTasks int) {
+	for i := 0; i < numOfTasks; i++ {
+		randnum := rand.Intn(999)
+		task := Task{i, randnum}
+		tasks <- task
+	}
+	close(tasks)
+}
+func getResult(done chan bool) {
+	for result := range results {
+		fmt.Printf("Task id %d, randnum %d , sum %d\n", result.task.id, result.task.randnum, result.result)
+	}
+	done <- true
+}
+func main() {
+	startTime := time.Now()
+	numOfWorkers := 20
+	numOfTasks := 100
+
+	var done = make(chan bool)
+	go getResult(done)
+	go allocate(numOfTasks)
+	go createWorkerPool(numOfWorkers)
+	// 必须在allocate()和getResult()之后创建工作池
+	<-done
+	endTime := time.Now()
+	diff := endTime.Sub(startTime)
+	fmt.Println("total time taken ", diff.Seconds(), "seconds")
 }
 
 type Trie struct {
