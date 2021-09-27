47a1874fa2864d3afee194e6048567bde0b8e5d3
xzg <790102426@qq.com>
2021-09-27 21:31:20 +0800

    update

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	a := []int{1, 2, 3, 4, 5, 6, 7}
@@ -8,21 +10,53 @@ func main() {
 	rotate(a, 6)
 	fmt.Println(a)
 }
-func rotate(nums []int, k int) {
-	length := len(nums)
-	k %= length //当k>length
-	index := length - k
-	copy(nums, append(nums[index:], nums[0:index]...))
-}
-func moveZeroes(nums []int) {
-	left, right, n := 0, 0, len(nums)
-	for right < n {
-		if nums[right] != 0 {
-			nums[left], nums[right] = nums[right], nums[left]
-			left++
+
+func intersect(nums1 []int, nums2 []int) []int {
+	//队列从小到大排序
+	quickSort(nums1)
+	quickSort(nums2)
+	var ret []int
+	//i:nums1下标,j:nums2下标
+	for i, j := 0, 0; i < len(nums1) && j < len(nums2); {
+		if nums1[i] == nums2[j] {
+			ret = append(ret, nums1[i])
+			i++
+			j++
+		} else if nums1[i] > nums2[j] {
+			j++
+		} else if nums1[i] < nums2[j] {
+			i++
 		}
-		right++
 	}
+	return ret
+}
+func quickSort(slice []int) {
+	var (
+		_quickSort func(left, right int, slice []int)     //利用递归不断对分区进行排序
+		partition  func(left, right int, slice []int) int //排序
+	)
+	partition = func(left, right int, slice []int) int {
+		flag := left      //基准
+		index := left + 1 //标记比slice[flag]大的位置
+		for i := index; i <= right; i++ {
+			if slice[i] < slice[flag] {
+				slice[i], slice[index] = slice[index], slice[i]
+				index++
+			}
+		}
+		slice[flag], slice[index-1] = slice[index-1], slice[flag]
+		return (index - 1)
+	}
+	_quickSort = func(left, right int, slice []int) {
+		if left < right {
+			partitionIndex := partition(left, right, slice) //排序并获取基准位置
+			//以基准位置进行分区，进行再排序
+			_quickSort(left, partitionIndex-1, slice)
+			_quickSort(partitionIndex+1, right, slice)
+		}
+	}
+	left, right := 0, len(slice)-1 //left起始值下标，right末尾值下标
+	_quickSort(left, right, slice)
 }
 
 type Trie struct {
