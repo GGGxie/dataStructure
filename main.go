13ed5db1d69e73a56082f210568e40d77164f6ba
ex_xiecg <790102426@qq.com>
2021-11-01 17:56:38 +0800

    添加扁平化迭代器

--- main.go
+++ main.go
@@ -5,40 +5,78 @@ import "fmt"
 // ["MinStack","push","push","push","getMin","pop","top","getMin"]
 // [[],[-2],[0],[-3],[],[],[],[]]
 func main() {
-	s := "32-1-1"
-	fmt.Println(calculate(s))
+	defer func() {
+		// if err := recover(); err != nil {
+		// 	fmt.Println("1111")
+		// }
+		fmt.Println("33333")
+	}()
+	panic("1")
+	fmt.Println("2222")
 }
 
-// https://leetcode-cn.com/problems/basic-calculator-ii/
-// 基本计算器 II
-// 解题思路：用数组模拟栈
-func calculate(s string) int {
-	var tempSlice []int //临时数组，记录所有待相加元素
-	var num int
-	preSign := '+'
-	for i, ch := range s {
-		isDigit := '0' <= ch && ch <= '9'
-		if isDigit { //记录数字
-			num = num*10 + int(ch-'0')
-		}
-		if (!isDigit && ch != ' ') || i == len(s)-1 { //ch为符号 || 遍历到最后一个字符
-			switch preSign {
-			case '+': //直接加入数组
-				tempSlice = append(tempSlice, num)
-			case '-': //将负数加入数组
-				tempSlice = append(tempSlice, -num)
-			case '*': // 将数组最后一个拿出来*num，将结果压入数组
-				tempSlice[len(tempSlice)-1] *= num
-			case '/': // 将数组最后一个拿出来/num，将结果压入数组
-				tempSlice[len(tempSlice)-1] /= num
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * type NestedInteger struct {
+ * }
+ *
+ * // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ * func (this NestedInteger) IsInteger() bool {}
+ *
+ * // Return the single integer that this NestedInteger holds, if it holds a single integer
+ * // The result is undefined if this NestedInteger holds a nested list
+ * // So before calling this method, you should have a check
+ * func (this NestedInteger) GetInteger() int {}
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * func (n *NestedInteger) SetInteger(value int) {}
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ * func (this *NestedInteger) Add(elem NestedInteger) {}
+ *
+ * // Return the nested list that this NestedInteger holds, if it holds a nested list
+ * // The list length is zero if this NestedInteger holds a single integer
+ * // You can access NestedInteger's List element directly if you want to modify it
+ * func (this NestedInteger) GetList() []*NestedInteger {}
+ */
+type NestedInteger struct {
+}
+
+func (this NestedInteger) GetList() []*NestedInteger {}
+func (this NestedInteger) GetInteger() int           {}
+func (this NestedInteger) IsInteger() bool           {}
+
+type NestedIterator struct {
+	vals []int
+}
+
+// nestedList看成一棵多根节点树，NestedInteger要么是叶子节点，要么是非叶子节点，非叶子用dfs遍历，叶子节点值加入迭代器数组
+func Constructor(nestedList []*NestedInteger) *NestedIterator {
+	var vals []int
+	var dfs func(ni []*NestedInteger)
+	dfs = func(ni []*NestedInteger) {
+		for _, nestedInteger := range ni {
+			if nestedInteger.IsInteger() {
+				vals = append(vals, nestedInteger.GetInteger())
+			} else {
+				dfs(nestedInteger.GetList())
 			}
-			preSign = ch
-			num = 0
 		}
 	}
-	var sum int
-	for i := range tempSlice { //将临时数组所有元素相加
-		sum += tempSlice[i]
+	dfs(nestedList)
+	return &NestedIterator{ //把得到的所有叶子节点值放进迭代器数组
+		vals: vals,
 	}
-	return sum
+}
+
+//每次返回第一个，并把第一个值排出对垒
+func (it *NestedIterator) Next() int {
+	val := it.vals[0]
+	it.vals = it.vals[1:]
+	return val
+}
+
+func (it *NestedIterator) HasNext() bool {
+	return len(it.vals) > 0
 }
---
+++ practice/tree/tree.go
@@ -0,0 +1,63 @@
+package tree
+
+// https://leetcode-cn.com/problems/flatten-nested-list-iterator/
+// 扁平化嵌套列表迭代器
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * type NestedInteger struct {
+ * }
+ *
+ * // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ * func (this NestedInteger) IsInteger() bool {}
+ *
+ * // Return the single integer that this NestedInteger holds, if it holds a single integer
+ * // The result is undefined if this NestedInteger holds a nested list
+ * // So before calling this method, you should have a check
+ * func (this NestedInteger) GetInteger() int {}
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * func (n *NestedInteger) SetInteger(value int) {}
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ * func (this *NestedInteger) Add(elem NestedInteger) {}
+ *
+ * // Return the nested list that this NestedInteger holds, if it holds a nested list
+ * // The list length is zero if this NestedInteger holds a single integer
+ * // You can access NestedInteger's List element directly if you want to modify it
+ * func (this NestedInteger) GetList() []*NestedInteger {}
+ */
+
+// type NestedIterator struct {
+// 	vals []int
+// }
+
+// // nestedList看成一棵多根节点树，NestedInteger要么是叶子节点，要么是非叶子节点，非叶子用dfs遍历，叶子节点值加入迭代器数组
+// func Constructor(nestedList []*NestedInteger) *NestedIterator {
+// 	var vals []int
+// 	var dfs func(ni []*NestedInteger)
+// 	dfs = func(ni []*NestedInteger) {
+// 		for _, nestedInteger := range ni {
+// 			if nestedInteger.IsInteger() {
+// 				vals = append(vals, nestedInteger.GetInteger())
+// 			} else {
+// 				dfs(nestedInteger.GetList())
+// 			}
+// 		}
+// 	}
+// 	dfs(nestedList)
+// 	return &NestedIterator{ //把得到的所有叶子节点值放进迭代器数组
+// 		vals: vals,
+// 	}
+// }
+
+// //每次返回第一个，并把第一个值排出对垒
+// func (it *NestedIterator) Next() int {
+// 	val := it.vals[0]
+// 	it.vals = it.vals[1:]
+// 	return val
+// }
+
+// func (it *NestedIterator) HasNext() bool {
+// 	return len(it.vals) > 0
+// }
