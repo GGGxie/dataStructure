4ebcf4435f6f00adee5b296d7aaca9c5f963e10d
ex_xiecg <790102426@qq.com>
2021-11-01 20:10:30 +0800

    添加逆波兰表达式求值

--- main.go
+++ main.go
@@ -1,82 +1,82 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // ["MinStack","push","push","push","getMin","pop","top","getMin"]
 // [[],[-2],[0],[-3],[],[],[],[]]
 func main() {
-	defer func() {
-		// if err := recover(); err != nil {
-		// 	fmt.Println("1111")
-		// }
-		fmt.Println("33333")
-	}()
-	panic("1")
-	fmt.Println("2222")
+	a := []string{"4", "13", "5", "/", "+"}
+	fmt.Println(evalRPN(a))
 }
 
-/**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * type NestedInteger struct {
- * }
- *
- * // Return true if this NestedInteger holds a single integer, rather than a nested list.
- * func (this NestedInteger) IsInteger() bool {}
- *
- * // Return the single integer that this NestedInteger holds, if it holds a single integer
- * // The result is undefined if this NestedInteger holds a nested list
- * // So before calling this method, you should have a check
- * func (this NestedInteger) GetInteger() int {}
- *
- * // Set this NestedInteger to hold a single integer.
- * func (n *NestedInteger) SetInteger(value int) {}
- *
- * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
- * func (this *NestedInteger) Add(elem NestedInteger) {}
- *
- * // Return the nested list that this NestedInteger holds, if it holds a nested list
- * // The list length is zero if this NestedInteger holds a single integer
- * // You can access NestedInteger's List element directly if you want to modify it
- * func (this NestedInteger) GetList() []*NestedInteger {}
- */
-type NestedInteger struct {
-}
-
-func (this NestedInteger) GetList() []*NestedInteger {}
-func (this NestedInteger) GetInteger() int           {}
-func (this NestedInteger) IsInteger() bool           {}
-
-type NestedIterator struct {
-	vals []int
-}
-
-// nestedList看成一棵多根节点树，NestedInteger要么是叶子节点，要么是非叶子节点，非叶子用dfs遍历，叶子节点值加入迭代器数组
-func Constructor(nestedList []*NestedInteger) *NestedIterator {
-	var vals []int
-	var dfs func(ni []*NestedInteger)
-	dfs = func(ni []*NestedInteger) {
-		for _, nestedInteger := range ni {
-			if nestedInteger.IsInteger() {
-				vals = append(vals, nestedInteger.GetInteger())
-			} else {
-				dfs(nestedInteger.GetList())
+// https://leetcode-cn.com/leetbook/read/top-interview-questions/xaqlgj/
+// 逆波兰表达式求值
+func evalRPN(tokens []string) int {
+	stack := NewStack(len(tokens))
+	for _, str := range tokens {
+		switch str {
+		case "+":
+			{
+				num1 := stack.Pop()
+				num2 := stack.Pop()
+				stack.Push(num1 + num2)
+			}
+		case "-":
+			{
+				num1 := stack.Pop()
+				num2 := stack.Pop()
+				stack.Push(num2 - num1)
+			}
+		case "*":
+			{
+				num1 := stack.Pop()
+				num2 := stack.Pop()
+				stack.Push(num1 * num2)
+			}
+		case "/":
+			{
+				num1 := stack.Pop()
+				num2 := stack.Pop()
+				stack.Push(num2 / num1)
+			}
+		default:
+			{
+				num, _ := strconv.ParseInt(str, 10, 64)
+				stack.Push(int(num))
 			}
 		}
 	}
-	dfs(nestedList)
-	return &NestedIterator{ //把得到的所有叶子节点值放进迭代器数组
-		vals: vals,
+	return stack.Pop()
+}
+
+type Stack struct {
+	cache  []int //存储数据，借助切片来排序
+	length int   //数组大小
+}
+
+func NewStack(size int) *Stack {
+	return &Stack{
+		cache:  make([]int, 0, size),
+		length: 0,
 	}
 }
 
-//每次返回第一个，并把第一个值排出对垒
-func (it *NestedIterator) Next() int {
-	val := it.vals[0]
-	it.vals = it.vals[1:]
-	return val
+// Push:往栈压入数据
+func (s *Stack) Push(value int) {
+	s.cache = append(s.cache, value)
+	s.length++
 }
 
-func (it *NestedIterator) HasNext() bool {
-	return len(it.vals) > 0
+// Pop:从栈取出数据
+func (s *Stack) Pop() int {
+	if s.length == 0 { //判断栈内是否有元素
+		return 0
+	}
+	ret := s.cache[s.length-1]        //获取栈顶元素
+	s.cache = s.cache[0 : s.length-1] //取出栈顶元素
+	s.length--
+	return ret
 }
--- practice/stack/stack.go
+++ practice/stack/stack.go
@@ -1,6 +1,9 @@
 package stack
 
-import "math"
+import (
+	"math"
+	"strconv"
+)
 
 // https://leetcode-cn.com/problems/min-stack/
 // 最小栈
@@ -49,4 +52,73 @@ func (this *MinStack) Top() int {
 
 func (this *MinStack) GetMin() int {
 	return this.minCache[len(this.minCache)-1]
+}
+
+// https://leetcode-cn.com/leetbook/read/top-interview-questions/xaqlgj/
+// 逆波兰表达式求值
+func evalRPN(tokens []string) int {
+	stack := NewStack(len(tokens))
+	for _, str := range tokens {
+		switch str { //遇到操作符就取出栈中两个数字进行计算
+		case "+":
+			{
+				num1 := stack.Pop()
+				num2 := stack.Pop()
+				stack.Push(num1 + num2)
+			}
+		case "-":
+			{
+				num1 := stack.Pop()
+				num2 := stack.Pop()
+				stack.Push(num2 - num1)
+			}
+		case "*":
+			{
+				num1 := stack.Pop()
+				num2 := stack.Pop()
+				stack.Push(num1 * num2)
+			}
+		case "/":
+			{
+				num1 := stack.Pop()
+				num2 := stack.Pop()
+				stack.Push(num2 / num1)
+			}
+		default: //遇到数字就压入栈中
+			{
+				num, _ := strconv.ParseInt(str, 10, 64)
+				stack.Push(int(num))
+			}
+		}
+	}
+	return stack.Pop()
+}
+
+type Stack struct {
+	cache  []int //存储数据，借助切片来排序
+	length int   //数组大小
+}
+
+func NewStack(size int) *Stack {
+	return &Stack{
+		cache:  make([]int, 0, size),
+		length: 0,
+	}
+}
+
+// Push:往栈压入数据
+func (s *Stack) Push(value int) {
+	s.cache = append(s.cache, value)
+	s.length++
+}
+
+// Pop:从栈取出数据
+func (s *Stack) Pop() int {
+	if s.length == 0 { //判断栈内是否有元素
+		return 0
+	}
+	ret := s.cache[s.length-1]        //获取栈顶元素
+	s.cache = s.cache[0 : s.length-1] //取出栈顶元素
+	s.length--
+	return ret
 }
