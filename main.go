5ade12503ec443b916eb793a50968633566d39f7
ex_xiecg <790102426@qq.com>
2021-11-02 16:39:21 +0800

    添加26进制转化为10进制

--- main.go
+++ main.go
@@ -2,81 +2,35 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 // ["MinStack","push","push","push","getMin","pop","top","getMin"]
 // [[],[-2],[0],[-3],[],[],[],[]]
 func main() {
-	a := []string{"4", "13", "5", "/", "+"}
-	fmt.Println(evalRPN(a))
+	fmt.Println(titleToNumber("AB"))
 }
 
-// https://leetcode-cn.com/leetbook/read/top-interview-questions/xaqlgj/
-// 逆波兰表达式求值
-func evalRPN(tokens []string) int {
-	stack := NewStack(len(tokens))
-	for _, str := range tokens {
-		switch str {
-		case "+":
-			{
-				num1 := stack.Pop()
-				num2 := stack.Pop()
-				stack.Push(num1 + num2)
-			}
-		case "-":
-			{
-				num1 := stack.Pop()
-				num2 := stack.Pop()
-				stack.Push(num2 - num1)
-			}
-		case "*":
-			{
-				num1 := stack.Pop()
-				num2 := stack.Pop()
-				stack.Push(num1 * num2)
-			}
-		case "/":
-			{
-				num1 := stack.Pop()
-				num2 := stack.Pop()
-				stack.Push(num2 / num1)
-			}
-		default:
-			{
-				num, _ := strconv.ParseInt(str, 10, 64)
-				stack.Push(int(num))
-			}
-		}
+// https://leetcode-cn.com/leetbook/read/top-interview-questions/xa6dkt/
+// 26进制转10进制
+func titleToNumber(columnTitle string) int {
+	var ret int
+	length := len(columnTitle)
+	for i, ch := range columnTitle {
+		temp := exponent(26, (length - i - 1)) //位数
+		temp2 := (int(ch-'A') + 1)             //最高位
+		ret += temp2 * temp
 	}
-	return stack.Pop()
-}
-
-type Stack struct {
-	cache  []int //存储数据，借助切片来排序
-	length int   //数组大小
-}
-
-func NewStack(size int) *Stack {
-	return &Stack{
-		cache:  make([]int, 0, size),
-		length: 0,
-	}
-}
-
-// Push:往栈压入数据
-func (s *Stack) Push(value int) {
-	s.cache = append(s.cache, value)
-	s.length++
-}
-
-// Pop:从栈取出数据
-func (s *Stack) Pop() int {
-	if s.length == 0 { //判断栈内是否有元素
-		return 0
-	}
-	ret := s.cache[s.length-1]        //获取栈顶元素
-	s.cache = s.cache[0 : s.length-1] //取出栈顶元素
-	s.length--
 	return ret
+}
+
+//计算a的n次方
+func exponent(a, n int) int {
+	result := int(1)
+	for i := n; i > 0; i >>= 1 {
+		if i&1 != 0 {
+			result *= a
+		}
+		a *= a
+	}
+	return result
 }
--- practice/slice/slice.go
+++ practice/slice/slice.go
@@ -261,3 +261,28 @@ func calculate(s string) int {
 	}
 	return sum
 }
+
+// https://leetcode-cn.com/leetbook/read/top-interview-questions/xa6dkt/
+// 26进制转10进制
+func titleToNumber(columnTitle string) int {
+	var ret int
+	length := len(columnTitle)
+	for i, ch := range columnTitle {
+		temp := exponent(26, (length - i - 1)) //位数
+		temp2 := (int(ch-'A') + 1)             //最高位
+		ret += temp2 * temp
+	}
+	return ret
+}
+
+//计算a的n次方
+func exponent(a, n int) int {
+	result := int(1)
+	for i := n; i > 0; i >>= 1 {
+		if i&1 != 0 {
+			result *= a
+		}
+		a *= a
+	}
+	return result
+}
