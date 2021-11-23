a94ae45a7a6dcd709b6c01d1493a342a69451545
ex_xiecg <790102426@qq.com>
2021-11-23 10:11:51 +0800

    二维数组中的查找

--- main.go
+++ main.go
@@ -5,17 +5,31 @@ import (
 )
 
 func main() {
-	fmt.Println(reverseLeftWords("abcdefg", 2))
+	z := [][]int{{1, 4, 7, 11, 15},
+		{2, 5, 8, 12, 19},
+		{3, 6, 9, 16, 22},
+		{10, 13, 14, 17, 24},
+		{18, 21, 23, 26, 30}}
+	fmt.Println(findNumberIn2DArray(z, 5))
 }
 
-// https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/
-// 寻找0～n-1中缺失的数字
-func missingNumber(nums []int) int {
-	length := len(nums)
-	for i := 0; i <= length; i++ {
-		if i == length || nums[i] != i {
-			return i
+// https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/submissions/
+// 二维数组中的查找
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	for i := range matrix {
+		//二分搜索
+		start, end := 0, len(matrix[i])-1
+		for start <= end {
+			mid := (start + end) / 2
+			fmt.Println(matrix[i][mid])
+			if matrix[i][mid] == target {
+				return true
+			} else if matrix[i][mid] > target {
+				end = mid - 1
+			} else if matrix[i][mid] < target {
+				start = mid + 1
+			}
 		}
 	}
-	return -1
+	return false
 }
--- practice/search/search.go
+++ practice/search/search.go
@@ -85,3 +85,23 @@ func missingNumber(nums []int) int {
 	}
 	return -1
 }
+
+// https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/submissions/
+// 二维数组中的查找
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	for i := range matrix {
+		//二分搜索
+		start, end := 0, len(matrix[i])-1
+		for start <= end {
+			mid := (start + end) / 2
+			if matrix[i][mid] == target {
+				return true
+			} else if matrix[i][mid] > target {
+				end = mid - 1
+			} else if matrix[i][mid] < target {
+				start = mid + 1
+			}
+		}
+	}
+	return false
+}
