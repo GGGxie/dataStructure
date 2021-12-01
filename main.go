5010cb5b41dc08fa49e25ceba01c418e1a41d95e
ex_xiecg <790102426@qq.com>
2021-12-01 14:26:48 +0800

    斐波那契数列

--- main.go
+++ main.go
@@ -2,27 +2,28 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sync/atomic"
-	"time"
 )
 
 var counter int32
 
 func main() {
-	counter = 0
-	for i := 0; i < 10000; i++ {
-		go func(i int) {
-			if atomic.CompareAndSwapInt32(&counter, counter, counter+1) {
-				time.Sleep(time.Duration(rand.Intn(2)) * time.Microsecond) //加一个睡眠
-				if counter > 3 {
-					return
-				}
-				fmt.Println(counter, "处理服务: ", i)
-				time.Sleep(time.Hour)
-			} else {
-			}
-		}(i)
+	fmt.Println(fib(45))
+}
+
+// https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
+// 斐波那契数列
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
 	}
-	time.Sleep(time.Hour)
+	temp1 := 0 //指向f(n-2)
+	temp2 := 1 //指向f(n-1)
+	for i := 2; i <= n; i++ {
+		temp3 := (temp1 + temp2) % 1000000007 //计算出f(n)
+		temp1 = temp2                         //f(n-2)指向之前f(n-1)的位置
+		temp2 = temp3                         //f(n-1)指向之前f(n)的位置
+	}
+	return temp2
 }
--- practice/math/math.go
+++ practice/math/math.go
@@ -127,3 +127,21 @@ func fizzBuzz(n int) []string {
 	}
 	return ret
 }
+
+// https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
+// 斐波那契数列
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	}
+	temp1 := 0 //指向f(n-2)
+	temp2 := 1 //指向f(n-1)
+	for i := 2; i <= n; i++ {
+		temp3 := (temp1 + temp2) % 1000000007 //计算出f(n)
+		temp1 = temp2                         //f(n-2)指向之前f(n-1)的位置
+		temp2 = temp3                         //f(n-1)指向之前f(n)的位置
+	}
+	return temp2
+}
