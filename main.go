729862d5c5836bdfdf38b8b4172553230126c4d3
ex_xiecg <790102426@qq.com>
2021-12-01 19:09:23 +0800

    最大利润

--- main.go
+++ main.go
@@ -2,42 +2,31 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 var counter int32
 
 func main() {
-	fmt.Println(numWays(7))
+	a := []int{7, 6}
+	fmt.Println(maxProfit1(a))
 }
 
-// https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/solution/go-gun-dong-shu-zu-by-xilepeng-x8wj
-// 青蛙跳台阶问题
-// 动态规划
-func numWays(n int) int {
-	if n == 0 {
-		return 1
-	} else if n == 1 {
-		return 1
-	} else if n == 2 {
-		return 2
+// https://leetcode-cn.com/problems/gu-piao-de-zui-da-li-run-lcof/submissions/
+// 股票的最大利润
+func maxProfit(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
 	}
-	dp := make([]int, n+1) //记录跳到i级台阶有几种方法
-	dp[0] = 1
-	dp[1] = 1
-	dp[2] = 2
-	for i := 3; i <= n; i++ {
-		dp[i] = (dp[i-1] + dp[i-2]) % 1000000007 //加法分配律？
+	min := math.MaxInt32 //维护一个最小值
+	ret := 0             //最大利润
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < min { //当但当前值比最小值大，最小值为当前值
+			min = prices[i]
+		}
+		if prices[i]-min > ret { //如果当前值-最小值>最大利润，则最大利润=当前值-最小值
+			ret = prices[i] - min
+		}
 	}
-	return dp[n]
+	return ret
 }
-
-// // //优化方案，空间为O(1)
-// func numWays(n int) int {
-// 	prev, curr := 1, 1
-// 	for i := 2; i <= n; i++ {
-// 		next := (prev + curr) % 1000000007
-// 		prev = curr
-// 		curr = next
-// 	}
-// 	return curr
-// }
--- practice/slice/slice.go
+++ practice/slice/slice.go
@@ -398,3 +398,22 @@ func canCompleteCircuit(gas []int, cost []int) int {
 	}
 	return -1
 }
+
+// https://leetcode-cn.com/problems/gu-piao-de-zui-da-li-run-lcof/submissions/
+// 股票的最大利润
+func maxProfit(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+	min := math.MaxInt32 //维护一个最小值
+	ret := 0             //最大利润
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < min { //当但当前值比最小值大，最小值为当前值
+			min = prices[i]
+		}
+		if prices[i]-min > ret { //如果当前值-最小值>最大利润，则最大利润=当前值-最小值
+			ret = prices[i] - min
+		}
+	}
+	return ret
+}
