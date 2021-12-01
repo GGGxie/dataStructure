93cd320c8f2914222c7e6bb37acf659bbcbf8c4b
ex_xiecg <790102426@qq.com>
2021-12-01 15:02:19 +0800

    青蛙跳台阶问题

--- main.go
+++ main.go
@@ -7,23 +7,37 @@ import (
 var counter int32
 
 func main() {
-	fmt.Println(fib(45))
+	fmt.Println(numWays(7))
 }
 
-// https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
-// 斐波那契数列
-func fib(n int) int {
+// https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/solution/go-gun-dong-shu-zu-by-xilepeng-x8wj
+// 青蛙跳台阶问题
+// 动态规划
+func numWays(n int) int {
 	if n == 0 {
-		return 0
+		return 1
 	} else if n == 1 {
 		return 1
+	} else if n == 2 {
+		return 2
 	}
-	temp1 := 0 //指向f(n-2)
-	temp2 := 1 //指向f(n-1)
-	for i := 2; i <= n; i++ {
-		temp3 := (temp1 + temp2) % 1000000007 //计算出f(n)
-		temp1 = temp2                         //f(n-2)指向之前f(n-1)的位置
-		temp2 = temp3                         //f(n-1)指向之前f(n)的位置
+	dp := make([]int, n+1) //记录跳到i级台阶有几种方法
+	dp[0] = 1
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = (dp[i-1] + dp[i-2]) % 1000000007 //加法分配律？
 	}
-	return temp2
+	return dp[n]
 }
+
+// // //优化方案，空间为O(1)
+// func numWays(n int) int {
+// 	prev, curr := 1, 1
+// 	for i := 2; i <= n; i++ {
+// 		next := (prev + curr) % 1000000007
+// 		prev = curr
+// 		curr = next
+// 	}
+// 	return curr
+// }
--- practice/dp/dp.go
+++ practice/dp/dp.go
@@ -220,3 +220,35 @@ func maxProduct(nums []int) int {
 	}
 	return maxNum
 }
+
+// https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/solution/go-gun-dong-shu-zu-by-xilepeng-x8wj
+// 青蛙跳台阶问题
+// 动态规划转移方程numWays(n)=numWays(n-1)+numWays(n-2)
+func numWays(n int) int {
+	if n == 0 {
+		return 1
+	} else if n == 1 {
+		return 1
+	} else if n == 2 {
+		return 2
+	}
+	dp := make([]int, n+1) //记录跳到i级台阶有几种方法
+	dp[0] = 1
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = (dp[i-1] + dp[i-2]) % 1000000007 //加法分配律？
+	}
+	return dp[n]
+}
+
+// // //优化方案，空间为O(1)
+// func numWays(n int) int {
+// 	prev, curr := 1, 1
+// 	for i := 2; i <= n; i++ {
+// 		next := (prev + curr) % 1000000007
+// 		prev = curr
+// 		curr = next
+// 	}
+// 	return curr
+// }
