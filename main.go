a692b7d72d5542d7675703c448099feac5f0ecda
ex_xiecg <790102426@qq.com>
2021-12-03 17:30:12 +0800

    把数字翻译成字符串

--- main.go
+++ main.go
@@ -1,39 +1,33 @@
 package main
 
 import (
-	_ "net/http/pprof"
+	"fmt"
+	"strconv"
 )
 
-func main() {}
+func main() {
+	fmt.Println(translateNum(18822))
+}
 
-// https://leetcode-cn.com/problems/li-wu-de-zui-da-jie-zhi-lcof/
-// 礼物的最大价值
-// 动态规划
-// 动态转移方程:maxValue(x, y) = max(maxValue(x-1,y)+grid[x][y],maxValue(x,y-1)+grid[x][y])
-func maxValue(grid [][]int) int {
-	var dp [][]int //记录和grid对应的每个点的最大价值
-	dp = make([][]int, len(grid))
-	for i := range dp {
-		dp[i] = make([]int, len(grid[i]))
-	}
-	for row := range grid {
-		for col := range grid[row] {
-			if row == 0 && col == 0 { //grid[0][0]
-				dp[0][0] = grid[0][0]
-			} else if row == 0 { //第一行
-				dp[row][col] = dp[row][col-1] + grid[row][col]
-			} else if col == 0 { //第一列
-				dp[row][col] = dp[row-1][col] + grid[row][col]
+// https://leetcode-cn.com/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof/
+// 把数字翻译成字符串
+// 动态规划，可以用滚动数组优化，待优化
+// dp[i]=dp[i-2]+dp[i-1] src[i-1:i+1] <= "25" && src[i-1:i+1] >= "10"
+// dp[i]=dp[i-1]//num[i] src[i-1:i+1] > "25" && src[i-1:i+1] < "10"
+func translateNum(num int) int {
+	src := strconv.Itoa(num)
+	dp := make([]int, len(src))
+	dp[0] = 1
+	for i := 1; i < len(src); i++ {
+		if src[i-1:i+1] <= "25" && src[i-1:i+1] >= "10" { //src[i-1:i+1]作为一个整体
+			if i == 1 {
+				dp[i] = dp[0] + 1
 			} else {
-				dp[row][col] = max(dp[row][col-1], dp[row-1][col]) + grid[row][col]
+				dp[i] = dp[i-1] + dp[i-2]
 			}
+		} else { //src[i]作为一个单体
+			dp[i] = dp[i-1]
 		}
 	}
-	return dp[len(grid)-1][len(grid[0])-1]
-}
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
+	return dp[len(src)-1]
 }
--- practice/dp/dp.go
+++ practice/dp/dp.go
@@ -2,6 +2,7 @@ package dp
 
 import (
 	"math"
+	"strconv"
 	"strings"
 )
 
@@ -277,4 +278,27 @@ func maxValue(grid [][]int) int {
 		}
 	}
 	return dp[len(grid)-1][len(grid[0])-1]
+}
+
+// https://leetcode-cn.com/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof/
+// 把数字翻译成字符串
+// 动态规划，可以用滚动数组优化，待优化
+// dp[i]=dp[i-2]+dp[i-1] src[i-1:i+1] <= "25" && src[i-1:i+1] >= "10"
+// dp[i]=dp[i-1]//num[i] src[i-1:i+1] > "25" && src[i-1:i+1] < "10"
+func translateNum(num int) int {
+	src := strconv.Itoa(num)
+	dp := make([]int, len(src))
+	dp[0] = 1
+	for i := 1; i < len(src); i++ {
+		if src[i-1:i+1] <= "25" && src[i-1:i+1] >= "10" { //src[i-1:i+1]作为一个整体
+			if i == 1 {
+				dp[i] = dp[0] + 1
+			} else {
+				dp[i] = dp[i-1] + dp[i-2]
+			}
+		} else { //src[i]作为一个单体
+			dp[i] = dp[i-1]
+		}
+	}
+	return dp[len(src)-1]
 }
