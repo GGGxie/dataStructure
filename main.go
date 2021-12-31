672465e82a0328ae6486be9de7dc809358ea5e9e
ex_xiecg <790102426@qq.com>
2021-12-31 18:16:17 +0800

    两个链表的第一个公共节点

--- main.go
+++ main.go
@@ -5,27 +5,20 @@ type ListNode struct {
 	Next *ListNode
 }
 
-// 合并两个排序的链表
-// https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/
-func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	idx1, idx2 := l1, l2
-	ret := &ListNode{} //ret指向新的链表的头结点，头结点不存数据，只为优化代码
-	idx3 := ret
-	for idx1 != nil && idx2 != nil {
-		if idx1.Val < idx2.Val {
-			idx3.Next = idx1
-			idx1 = idx1.Next
-		} else {
-			idx3.Next = idx2
-			idx2 = idx2.Next
+// https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
+// 两个链表的第一个公共节点
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	mapp := make(map[*ListNode]struct{})
+	idx1, idx2 := headA, headB
+	for idx1 != nil { //把headA指向的链表存入map中
+		mapp[idx1] = struct{}{}
+		idx1 = idx1.Next
+	}
+	for idx2 != nil { //找出headB指向的链表中第一个存在于map中的节点，这就是第一个公共节点
+		if _, ok := mapp[idx2]; ok {
+			return idx2
 		}
-		idx3 = idx3.Next
+		idx2 = idx2.Next
 	}
-	if idx1 != nil {
-		idx3.Next = idx1
-	}
-	if idx2 != nil {
-		idx3.Next = idx2
-	}
-	return ret.Next //返回头结点.Next
+	return nil
 }
--- practice/list/list.go
+++ practice/list/list.go
@@ -133,6 +133,24 @@ func getIntersectionNode(headA, headB *ListNode) *ListNode {
 	return nil
 }
 
+// // https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
+// // 两个链表的第一个公共节点
+// func getIntersectionNode(headA, headB *ListNode) *ListNode {
+// 	mapp := make(map[*ListNode]struct{})
+// 	idx1, idx2 := headA, headB
+// 	for idx1 != nil { //把headA指向的链表存入map中
+// 		mapp[idx1] = struct{}{}
+// 		idx1 = idx1.Next
+// 	}
+// 	for idx2 != nil { //找出headB指向的链表中第一个存在于map中的节点，这就是第一个公共节点
+// 		if _, ok := mapp[idx2]; ok {
+// 			return idx2
+// 		}
+// 		idx2 = idx2.Next
+// 	}
+// 	return nil
+// }
+
 // https://leetcode-cn.com/leetbook/read/top-interview-questions/xavip3/
 // 翻转链表
 func reverseList(head *ListNode) *ListNode {
